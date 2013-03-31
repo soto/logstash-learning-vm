f42643bf80a0d855cd7268e3e72636d43166173d
Damian Soto <soto@coderev.org>
2013-04-01 00:50:39 +0200

    add missing ruby dependencies,force apt-get update
    add missing ruby dependencies for rvm install ruby-1.9.3
    hack to force apt-get update
    apt-get update should probably go in a different module

--- modules/rvm/manifests/install.pp
+++ modules/rvm/manifests/install.pp
@@ -5,6 +5,28 @@ define rvm::install (
   include rvm::files
   include curl
 
+  package {
+    # rvm complains it wants these packages for ruby1.9.3
+  	"libreadline6-dev": ensure => latest;
+  	"libsqlite3-dev": ensure => latest;
+  	"sqlite3": ensure => latest;	
+    "libxml2-dev": ensure => latest;
+  	"libxslt1-dev": ensure => latest;
+  	"autoconf": ensure => latest;
+  	"libgdbm-dev": ensure => latest;
+  	"libncurses5-dev": ensure => latest;
+  	"automake": ensure => latest;
+  	"libtool": ensure => latest;
+  	"bison": ensure => latest;
+  	"libffi-dev": ensure => latest;
+  } 
+
+exec { "apt-update":
+    command => "/usr/bin/apt-get update"
+}
+
+Exec["apt-update"] -> Package <| |>
+
   exec {
     "get rvm for $user":
       require => [User[$user], Class["rvm::files"], Class["curl"]],
